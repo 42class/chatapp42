a326947d2da5b2c24654b9a1c4d08b6c82b8b22a
kei-nishikawa <70260848+kei-nishikawa48@users.noreply.github.com>
2021-01-09 22:12:56 +0900

    Revert "ItemコンポーネントProps受け取りとデザイン作成"

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,5 @@
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-
-const useStyles = makeStyles({
-  card: {
-    margin: "15px",
-    padding: "15px",
-    backgroundColor: "#7fbfff",
-  },
-});
-
-const Item = ({ user, content }) => {
-  const classes = useStyles();
-  return <Card className={classes.card}>{`${user} : ${content}`}</Card>;
+const Item = () => {
+  return <h1>Item</h1>;
 };
 
 export default Item;
