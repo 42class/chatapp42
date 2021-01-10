9bb663bebcfac1dbbf7f561e5e4e9fdc2dea288c
kei <xnkcelebrate48x@yahoo.co.jp>
2021-01-10 18:31:13 +0900

    修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-import SignIn from './pages/SignIn'
 
-
-const App = () => {
-
-    return (
-        <>
-            <SignIn />
-        </>
-    )
-}
-
-export default App
-=======
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Room from "./pages/Room";
 import SignUp from "./pages/SignUp";
@@ -30,4 +16,4 @@ const App = () => {
   );
 };
 export default App;
->>>>>>> b75104e848f3cb637a4b424ad7b1cb62be0fe74b
+
